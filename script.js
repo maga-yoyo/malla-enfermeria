52b589c8f431c133ced01b549af873718fcfe6a7
maga-yoyo <magatabares40@gmail.com>
2025-07-13 17:19:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,12 @@
-const container = document.getElementById("materias-container");
-const progresoGuardado = JSON.parse(localStorage.getItem("progresoEnfermeria") || "[]");
-
-function crearMateria(materia) {
-  const div = document.createElement("div");
-  div.classList.add("materia");
-  div.innerText = materia.nombre;
-  div.dataset.id = materia.id;
-  container.appendChild(div);
-}
-
-function estaAprobada(id) {
-  return progresoGuardado.includes(id);
-}
-
-function sePuedeDesbloquear(materia) {
-  return materia.correlativas.every(correlativa => progresoGuardado.includes(correlativa));
-}
-
-function renderMaterias() {
-  container.innerHTML = "";
-  materias.forEach(materia => {
+document.addEventListener("DOMContentLoaded", () => {
+  const malla = document.getElementById("malla");
+  materias.forEach((materia, index) => {
     const div = document.createElement("div");
-    div.classList.add("materia");
-    div.innerText = materia.nombre;
-    div.dataset.id = materia.id;
-
-    const aprobada = estaAprobada(materia.id);
-    const desbloqueada = sePuedeDesbloquear(materia);
-
-    if (aprobada) {
-      div.classList.add("aprobada");
-    } else if (!desbloqueada && materia.correlativas.length > 0) {
-      div.classList.add("bloqueada");
-    }
-
+    div.className = "materia";
+    div.textContent = materia.nombre;
     div.addEventListener("click", () => {
-      if (!div.classList.contains("bloqueada")) {
-        if (estaAprobada(materia.id)) {
-          const index = progresoGuardado.indexOf(materia.id);
-          if (index > -1) progresoGuardado.splice(index, 1);
-        } else {
-          progresoGuardado.push(materia.id);
-        }
-        localStorage.setItem("progresoEnfermeria", JSON.stringify(progresoGuardado));
-        renderMaterias();
-      }
+      div.classList.toggle("aprobada");
     });
-
-    container.appendChild(div);
+    malla.appendChild(div);
   });
-}
-
-renderMaterias();
+});
