178a45ec0be5359be77199472fde6c3d2353fbbe
maga-yoyo <magatabares40@gmail.com>
2025-07-13 16:28:15 -0300

    Create materias.js

---
+++ materias.js
@@ -0,0 +1,356 @@
+[
+  {
+    "nombre": "Fundamentos y Bases Filosoficas del Cuidado",
+    "codigo": "SA02001",
+    "cuatrimestre": 1,
+    "correlativas": []
+  },
+  {
+    "nombre": "Condiciones y Medio Ambiente del Trabajo en Salud",
+    "codigo": "SA02002",
+    "cuatrimestre": 1,
+    "correlativas": []
+  },
+  {
+    "nombre": "Salud Publica",
+    "codigo": "SA02003",
+    "cuatrimestre": 1,
+    "correlativas": []
+  },
+  {
+    "nombre": "Biologia Humana I",
+    "codigo": "SA02004",
+    "cuatrimestre": 1,
+    "correlativas": []
+  },
+  {
+    "nombre": "Microbiologia y Parasitologia",
+    "codigo": "SA02005",
+    "cuatrimestre": 1,
+    "correlativas": []
+  },
+  {
+    "nombre": "Practica Profesionalizante I",
+    "codigo": "SA02006",
+    "cuatrimestre": 1,
+    "correlativas": []
+  },
+  {
+    "nombre": "Trabajo Social Comunitario I",
+    "codigo": "TSC01",
+    "cuatrimestre": 1,
+    "correlativas": []
+  },
+  {
+    "nombre": "Cuidados de la Salud Centrados en la Comunidad",
+    "codigo": "SA02008",
+    "cuatrimestre": 2,
+    "correlativas": ["SA02001", "SA02003"]
+  },
+  {
+    "nombre": "Introduccion a la Nutricion Saludable",
+    "codigo": "SA02009",
+    "cuatrimestre": 2,
+    "correlativas": ["SA02002"]
+  },
+  {
+    "nombre": "Biologia Humana II",
+    "codigo": "SA02010",
+    "cuatrimestre": 2,
+    "correlativas": ["SA02004"]
+  },
+  {
+    "nombre": "Epidemiologia y Salud Publica",
+    "codigo": "SA02011",
+    "cuatrimestre": 2,
+    "correlativas": ["SA02003"]
+  },
+  {
+    "nombre": "Practica Profesionalizante II",
+    "codigo": "SA02012",
+    "cuatrimestre": 2,
+    "correlativas": ["SA02006"]
+  },
+  {
+    "nombre": "Informatica I",
+    "codigo": "TRI01",
+    "cuatrimestre": 2,
+    "correlativas": []
+  },
+  {
+    "nombre": "Antropologia y Sociedad",
+    "codigo": "SA02014",
+    "cuatrimestre": 2,
+    "correlativas": []
+  },
+  {
+    "nombre": "Aspectos Legales y Deontologicos de Enfermeria",
+    "codigo": "SA02015",
+    "cuatrimestre": 3,
+    "correlativas": []
+  },
+  {
+    "nombre": "Farmacologia en Enfermeria",
+    "codigo": "SA02016",
+    "cuatrimestre": 3,
+    "correlativas": []
+  },
+  {
+    "nombre": "Enfermeria Materno infantil I",
+    "codigo": "SA02017",
+    "cuatrimestre": 3,
+    "correlativas": []
+  },
+  {
+    "nombre": "Practica Profesionalizante III",
+    "codigo": "SA02018",
+    "cuatrimestre": 3,
+    "correlativas": ["SA02012"]
+  },
+  {
+    "nombre": "Trabajo Social Comunitario II",
+    "codigo": "TSC02",
+    "cuatrimestre": 3,
+    "correlativas": ["TSC01"]
+  },
+  {
+    "nombre": "Enfermeria del Adulto y Adulto Mayor",
+    "codigo": "SA02020",
+    "cuatrimestre": 3,
+    "correlativas": []
+  },
+  {
+    "nombre": "Sociologia y Enfermeria",
+    "codigo": "SA02021",
+    "cuatrimestre": 3,
+    "correlativas": []
+  },
+  {
+    "nombre": "Cuidados Maternales en Situacion de Riesgo",
+    "codigo": "SA02022",
+    "cuatrimestre": 4,
+    "correlativas": ["SA02017"]
+  },
+  {
+    "nombre": "Enfermeria del Niño y Adolescente",
+    "codigo": "SA02023",
+    "cuatrimestre": 4,
+    "correlativas": []
+  },
+  {
+    "nombre": "Psicosociologia de las Instituciones de Salud",
+    "codigo": "SA02024",
+    "cuatrimestre": 4,
+    "correlativas": []
+  },
+  {
+    "nombre": "Practica Profesionalizante IV",
+    "codigo": "SA02025",
+    "cuatrimestre": 4,
+    "correlativas": ["SA02018"]
+  },
+  {
+    "nombre": "Epidemiologia y Enfermeria",
+    "codigo": "SA02026",
+    "cuatrimestre": 4,
+    "correlativas": ["SA02011"]
+  },
+  {
+    "nombre": "Nutricion y Dietoterapia",
+    "codigo": "SA02027",
+    "cuatrimestre": 4,
+    "correlativas": ["SA02009"]
+  },
+  {
+    "nombre": "Filosofia y Derechos Humanos en Salud",
+    "codigo": "SA02028",
+    "cuatrimestre": 4,
+    "correlativas": []
+  },
+  {
+    "nombre": "Enfermeria Comunitaria",
+    "codigo": "SA02029",
+    "cuatrimestre": 5,
+    "correlativas": []
+  },
+  {
+    "nombre": "Psicologia y Comunicacion en Salud",
+    "codigo": "SA02030",
+    "cuatrimestre": 5,
+    "correlativas": []
+  },
+  {
+    "nombre": "Practica Profesionalizante V",
+    "codigo": "SA02031",
+    "cuatrimestre": 5,
+    "correlativas": ["SA02025"]
+  },
+  {
+    "nombre": "Enfermeria en Salud Mental e Intervenciones Comunitarias",
+    "codigo": "SA02032",
+    "cuatrimestre": 5,
+    "correlativas": []
+  },
+  {
+    "nombre": "Trabajo Social Comunitario III",
+    "codigo": "TSC03",
+    "cuatrimestre": 5,
+    "correlativas": ["TSC02"]
+  },
+  {
+    "nombre": "Ingles I",
+    "codigo": "TRL01",
+    "cuatrimestre": 5,
+    "correlativas": []
+  },
+  {
+    "nombre": "Organizacion y Gestion de Servicios de Enfermeria",
+    "codigo": "SA02035",
+    "cuatrimestre": 6,
+    "correlativas": []
+  },
+  {
+    "nombre": "Enfermeria Materno Infantil II",
+    "codigo": "SA02036",
+    "cuatrimestre": 6,
+    "correlativas": ["SA02017"]
+  },
+  {
+    "nombre": "Introduccion a la Investigacion en Enfermeria",
+    "codigo": "SA02037",
+    "cuatrimestre": 6,
+    "correlativas": []
+  },
+  {
+    "nombre": "Practica Profesionalizante Integral I",
+    "codigo": "SA02038",
+    "cuatrimestre": 6,
+    "correlativas": ["SA02031"]
+  },
+  {
+    "nombre": "Cuidados Pediatricos en Situacion de Riesgo",
+    "codigo": "SA02039",
+    "cuatrimestre": 6,
+    "correlativas": ["SA02023"]
+  },
+  {
+    "nombre": "Metodologia de la Investigacion",
+    "codigo": "SA02041",
+    "cuatrimestre": 7,
+    "correlativas": []
+  },
+  {
+    "nombre": "Etica y Deontologia Profesional de Enfermeria",
+    "codigo": "SA02042",
+    "cuatrimestre": 7,
+    "correlativas": []
+  },
+  {
+    "nombre": "Gestion de Cuidados Comunitarios y Hospitalarios I",
+    "codigo": "SA02043",
+    "cuatrimestre": 7,
+    "correlativas": []
+  },
+  {
+    "nombre": "Trabajo Social Comunitario IV",
+    "codigo": "TSC04",
+    "cuatrimestre": 7,
+    "correlativas": ["TSC03"]
+  },
+  {
+    "nombre": "Epistemologia y Filosofia del Cuidado",
+    "codigo": "SA02045",
+    "cuatrimestre": 7,
+    "correlativas": []
+  },
+  {
+    "nombre": "Educacion en Salud y Enfermeria",
+    "codigo": "SA02046",
+    "cuatrimestre": 7,
+    "correlativas": []
+  },
+  {
+    "nombre": "Enfermeria en Cuidados Criticos",
+    "codigo": "SA02047",
+    "cuatrimestre": 7,
+    "correlativas": []
+  },
+  {
+    "nombre": "Seminario de Tesis e Investigacion en Enfermeria",
+    "codigo": "SA02048",
+    "cuatrimestre": 8,
+    "correlativas": ["SA02041"]
+  },
+  {
+    "nombre": "Marcos Legales y Profesionales en Enfermeria",
+    "codigo": "SA02049",
+    "cuatrimestre": 8,
+    "correlativas": []
+  },
+  {
+    "nombre": "Sociologia, Salud y Enfermeria",
+    "codigo": "SA02050",
+    "cuatrimestre": 8,
+    "correlativas": []
+  },
+  {
+    "nombre": "Enfermeria y Practicas Comunitaria",
+    "codigo": "SA02051",
+    "cuatrimestre": 8,
+    "correlativas": []
+  },
+  {
+    "nombre": "Filosofia y Ciencia Enfermera",
+    "codigo": "SA02052",
+    "cuatrimestre": 8,
+    "correlativas": []
+  },
+  {
+    "nombre": "Educacion en Enfermeria",
+    "codigo": "SA02053",
+    "cuatrimestre": 8,
+    "correlativas": []
+  },
+  {
+    "nombre": "Taller de Tesis I",
+    "codigo": "SA02054",
+    "cuatrimestre": 9,
+    "correlativas": ["SA02048"]
+  },
+  {
+    "nombre": "Practica Profesionalizante Integral II",
+    "codigo": "SA02055",
+    "cuatrimestre": 9,
+    "correlativas": ["SA02038"]
+  },
+  {
+    "nombre": "Gestion de Cuidados Comunitarios y Hospitalarios II",
+    "codigo": "SA02056",
+    "cuatrimestre": 9,
+    "correlativas": ["SA02043"]
+  },
+  {
+    "nombre": "Cuidados Criticos en el Adulto Mayor",
+    "codigo": "SA02057",
+    "cuatrimestre": 9,
+    "correlativas": []
+  },
+  {
+    "nombre": "Taller de Tesis II",
+    "codigo": "SA02058",
+    "cuatrimestre": 10,
+    "correlativas": ["SA02054"]
+  },
+  {
+    "nombre": "Sociologia y Sociedad",
+    "codigo": "SA02059",
+    "cuatrimestre": 10,
+    "correlativas": []
+  },
+  {
+    "nombre": "Practica Profesionalizante en Cuidados Criticos",
+    "codigo": "SA02060",
+    "cuatrimestre": 10,
+    "correlativas": ["SA02047"]
+  }
+]
